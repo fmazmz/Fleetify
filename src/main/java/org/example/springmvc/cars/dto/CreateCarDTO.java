2e1d9d656094ad0200a193a60ea88104c2b25e76
WHITEROSE <firasmoussa60@gmail.com>
2026-03-05 02:43:32 +0100

    add jakarta validation in DTO

--- src/main/java/org/example/springmvc/cars/dto/CreateCarDTO.java
+++ src/main/java/org/example/springmvc/cars/dto/CreateCarDTO.java
@@ -1,10 +1,19 @@
 package org.example.springmvc.cars.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
 import java.time.Year;
 
 public record CreateCarDTO(
+        @NotBlank(message = "Make is required")
         String make,
+
+        @NotBlank(message = "Model is required")
         String model,
+
+        @NotBlank(message = "Year is required")
+        @PastOrPresent
         Year year
 ) {
 }
