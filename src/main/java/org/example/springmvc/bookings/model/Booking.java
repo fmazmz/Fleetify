4a3aab7032a47d4b22a127f56c1a1210d068032e
Kristina M <kristina0x7@gmail.com>
2026-03-13 22:38:23 +0100

    improve entity

--- src/main/java/org/example/springmvc/bookings/model/Booking.java
+++ src/main/java/org/example/springmvc/bookings/model/Booking.java
@@ -15,6 +15,11 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@Table(
+        indexes = {
+                @Index(name = "idx_booking_car_time", columnList = "car_id,startTime,endTime")
+        }
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,27 +29,42 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "car_id")
     private Car car;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "driver_id")
     private Driver driver;
 
+    @Column(nullable = false)
     private Instant startTime;
+
+    @Column(nullable = false)
     private Instant endTime;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private InsuranceType insuranceType;
 
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal totalPrice;
 
     @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private Instant createdAt;
 
     @UpdateTimestamp
     private Instant updatedAt;
 
-    public Booking(Car car, Driver driver, Instant startTime, Instant endTime, InsuranceType insuranceType, BigDecimal totalPrice) {
+    public Booking(
+            Car car,
+            Driver driver,
+            Instant startTime,
+            Instant endTime,
+            InsuranceType insuranceType,
+            BigDecimal totalPrice
+    ) {
         this.car = car;
         this.driver = driver;
         this.startTime = startTime;
